dddc6a79bf9e8a97ddf6a98ef2cc1821bd0d0368
MathyouMB <matthewmacraebovell@bell.net>
2020-10-07 22:19:30 -0400

    added purchase model spec

---
+++ spec/models/purchase_spec.rb
@@ -0,0 +1,33 @@
+require 'rails_helper'
+
+RSpec.describe(Purchase, type: :model) do
+  let(:purchase) { build(:purchase) }
+
+  describe 'Purchase Attribute Validations' do
+    let(:result) { purchase.valid? }
+
+    context 'When purchase has no title' do
+      before { purchase.title = nil }
+
+      it 'returns false' do
+        expect(result).to(be(false))
+      end
+    end
+
+    context 'When purchase has no cost' do
+      before { purchase.cost = nil }
+
+      it 'returns false' do
+        expect(result).to(be(false))
+      end
+    end
+
+    context 'When purchase has negative cost' do
+      before { purchase.cost = -1 }
+
+      it 'returns false' do
+        expect(result).to(be(false))
+      end
+    end
+  end
+end
